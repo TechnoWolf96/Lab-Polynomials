da3356e12a6e6cbe97a7e6d88f1f0d0a50081a56
TechnoWolf96 <v.alentin52.v@yandex.ru>
2022-02-14 08:41:55 +0300

    Написаны методы TList

--- Lab-Polynomials/TList.h
+++ Lab-Polynomials/TList.h
@@ -12,8 +12,8 @@ public:
 	~TList();
 	bool IsEmpty() { return length == 0 };
 	void InsertFirst(T item);
-	void InsertCurrent();
-	void InsertLast();
+	void InsertCurrent(T item);
+	void InsertLast(T item);
 	void DeleteFirst();
 	void DeleteCurrent();
 
@@ -29,6 +29,12 @@ inline TList<T>::TList()
 }
 
 template<class T>
+inline TList<T>::~TList()
+{
+	while (length != 0) DeleteFirst();
+}
+
+template<class T>
 inline void TList<T>::InsertFirst(T item)
 {
 	TNode<T>* newNode = new TNode<T>();
@@ -40,9 +46,48 @@ inline void TList<T>::InsertFirst(T item)
 }
 
 template<class T>
-inline void TList<T>::InsertLast()
+inline void TList<T>::InsertCurrent(T item)
 {
+	if (pCurrent == pFirst) {InsertFirst(); return;}
+	if (pCurrent == pLast) { InsertLast(); return; }
+	TNode<T>* newNode = new TNode<T>();
+	newNode->value = item;
+	newNode->pNext = pCurrent;
+	pPrevious->pNext = newNode;
+	length++;
+}
 
+template<class T>
+inline void TList<T>::InsertLast(T item)
+{
+	if (IsEmpty()) { InsertFirst(item); return; }
+	TNode<T>* newNode = new TNode<T>();
+	newNode->value = item;
+	newNode->pNext = nullptr;
+	pLast->pNext = newNode;
+	length++;
+}
+
+template<class T>
+inline void TList<T>::DeleteFirst()
+{
+	if (length == 0) throw "List is empty";
+	TNode<T>* pDeletable = pFirst;
+	pFirst = pFirst->pNext;
+	delete* pDeletable;
+	length--;
+
+}
+
+template<class T>
+inline void TList<T>::DeleteCurrent()
+{
+	if (length == 0) throw "List is empty";
+	TNode<T>* pDeletable = pCurrent;
+	pCurrent = pCurrent->pNext;
+	pPrevious->pNext = pCurrent;
+	delete* pDeletable;
+	length--;
 }
 
 
--- Lab-Polynomials/TMonom.h
+++ Lab-Polynomials/TMonom.h
@@ -4,7 +4,28 @@ struct TMonom
 	double coef;
 	int degX, degY, degZ;
 
-	bool operator==(const TMonom &other)
+	bool operator==(const TMonom& other)
+	{
+		return coef == other.coef && degX == other.degX && degY == other.degY && degZ == other.degZ;
+	}
+	
+	bool operator>(const TMonom& other)
+	{
+		if (degX > other.degX) return true;
+		else if (degX == other.degX && degY > other.degY) return true;
+		else if (degX == other.degX && degY == other.degY && degZ > other.degZ) return true;
+		else if (degX == other.degX && degY == other.degY && degZ == other.degZ && coef > other.coef) return true;
+		return false;
+	}
+
+	bool operator<(const TMonom& other)
+	{
+		if (degX < other.degX) return true;
+		else if (degX == other.degX && degY < other.degY) return true;
+		else if (degX == other.degX && degY == other.degY && degZ < other.degZ) return true;
+		else if (degX == other.degX && degY == other.degY && degZ == other.degZ && coef < other.coef) return true;
+		return false;
+	}
 
 
 };
